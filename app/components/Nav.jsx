7d170e614c5dc9c725981e92fb1cd57505159313
Thor Erik Lie <thor@thorerik.com>
2016-10-28 23:30:44 +0200

    Pages + styles

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -7,8 +7,8 @@ var Nav = () => {
         <div className="top-bar-left">
           <ul className="menu">
             <li className="menu-text">Timer appety-app</li>
-            <li><IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Timer</IndexLink></li>
-            <li><Link to="/countdown" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Countdown</Link></li>
+            <li><IndexLink to="/" activeClassName="active">Timer</IndexLink></li>
+            <li><Link to="/countdown" activeClassName="active">Countdown</Link></li>
           </ul>
         </div>
         <div className="top-bar-right">
