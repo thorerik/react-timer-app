a264104172fa3374abf12f78e12667b893239df7
Thor Erik Lie <thor@thorerik.com>
2016-10-31 09:45:09 +0100

    moar tests

--- app/tests/components/Timer.test.jsx
+++ app/tests/components/Timer.test.jsx
@@ -10,19 +10,48 @@ describe('Timer', () => {
     it('should exist', () => {
         expect(Timer).toExist();
     });
-    describe('hanndle startTimer', () => {
-        it('should set state to started and count up', (done) => {
-            var timer = TestUtils.renderIntoDocument(<Timer/>);
-            timer.handleStatusChange('started');
+    
+    it('should set state to started and count up', (done) => {
+        var timer = TestUtils.renderIntoDocument(<Timer/>);
+        timer.handleStatusChange('started');
 
 
+        expect(timer.state.count).toBe(0);
+        expect(timer.state.timerStatus).toBe('started');
+
+        setTimeout(() => {
+            expect(timer.state.count).toBe(1);
+            done();
+        }, 1001);
+    });
+    
+    it('should set state to paused and not count up', (done) => {
+        var timer = TestUtils.renderIntoDocument(<Timer/>);
+
+        timer.setState({count: 10});
+        timer.handleStatusChange('started');
+        timer.handleStatusChange('paused');
+        
+
+        setTimeout(() => {
+            expect(timer.state.timerStatus).toBe('paused');
+            expect(timer.state.count).toBe(10);
+            done();
+        }, 1001);
+    });
+    
+    it('should set state to stopped and clear count', (done) => {
+        var timer = TestUtils.renderIntoDocument(<Timer/>);
+
+        timer.setState({count: 10});
+        timer.handleStatusChange('started');
+        timer.handleStatusChange('stopped');
+        
+
+        setTimeout(() => {
+            expect(timer.state.timerStatus).toBe('stopped');
             expect(timer.state.count).toBe(0);
-            expect(timer.state.timerStatus).toBe('started');
-
-            setTimeout(() => {
-                expect(timer.state.count).toBe(1);
-                done();
-            }, 1001);
-        });
+            done();
+        }, 1001);
     });
 });
